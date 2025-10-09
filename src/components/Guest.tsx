e6b81d1e4e2af737eedafadd61421946351fbfe8
Ravi Kant Jha <ravirbikant@gmail.com>
2025-10-09 12:09:12 +0530

    Improving Host page

--- src/components/Guest.tsx
+++ src/components/Guest.tsx
@@ -1,5 +1,34 @@
-const Guest = () => {
-  return <>Guest Screen</>;
+import { useRef, useEffect } from "react";
+
+const Guest = (): JSX.Element => {
+  const localFeed = useRef<HTMLVideoElement>(null);
+  const localStream = useRef<MediaStream | null>(null);
+
+  useEffect(() => {
+    const getLocalFeed = async (): Promise<void> => {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        localStream.current = stream;
+        if (localFeed.current) {
+          localFeed.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.log("Error : ", error);
+      }
+    };
+
+    getLocalFeed();
+  }, []);
+
+  return (
+    <>
+      <div>
+        <video ref={localFeed} autoPlay playsInline muted />
+      </div>
+    </>
+  );
 };
 
 export default Guest;
--- src/components/Host.tsx
+++ src/components/Host.tsx
@@ -1,5 +1,50 @@
-const Host = () => {
-  return <>Host Screen</>;
+import { useRef, useEffect, useState } from "react";
+import "./styles.css";
+
+const Host = (): JSX.Element => {
+  const localFeed = useRef<HTMLVideoElement>(null);
+  const localStream = useRef<MediaStream | null>(null);
+  const [isVideoOn, setIsVideoOn] = useState<booleam>(false);
+
+  useEffect(() => {
+    const getLocalFeed = async (): Promise<void> => {
+      try {
+        if (!isVideoOn) return;
+        const stream = await navigator.mediaDevices.getUserMedia({
+          video: true,
+        });
+        localStream.current = stream;
+        if (localFeed.current) {
+          localFeed.current.srcObject = stream;
+        }
+      } catch (error) {
+        console.log("Error : ", error);
+      }
+    };
+
+    getLocalFeed();
+
+    return () => {
+      localStream.current?.getTracks().forEach((track) => track.stop());
+    };
+  }, [isVideoOn]);
+
+  return (
+    <>
+      <div>
+        <div className="local-video-container">
+          <video ref={localFeed} autoPlay playsInline muted />
+        </div>
+        <button
+          onClick={() => {
+            setIsVideoOn((prev) => !prev);
+          }}
+        >
+          Turn Video {isVideoOn ? "off" : "on"}
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Host;
