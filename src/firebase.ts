b61598701780eede86add2718a4b9bafb8dc8485
Ravi Kant Jha <ravirbikant@gmail.com>
2025-10-16 23:19:43 +0530

    Adding firebase for signalling

---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCRTIYY_XviLB26GedZCPfgTphoy0bL9LM",
+  authDomain: "my-oreo-cam.firebaseapp.com",
+  projectId: "my-oreo-cam",
+  storageBucket: "my-oreo-cam.firebasestorage.app",
+  messagingSenderId: "26332160183",
+  appId: "1:26332160183:web:548bdae10d0e88de826d32",
+  measurementId: "G-X77WW7MXNS",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
