8aa96f3ab1068ff354a8affba2f3fe4cbc78cade
Ravi Kant Jha <ravirbikant@gmail.com>
2025-12-25 13:08:40 +0530

    Removing home screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Guest from "./components/Guest";
 import Host from "./components/Host";
-import Home from "./components/Home";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/host" element={<Host />} />
       <Route path="/guest" element={<Guest />} />
+      <Route path="*" element={<Host />} />
     </Routes>
   );
 }
